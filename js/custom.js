e0134ad04c6384ac2922fbbdebe5472ddefc2c91
Ecenys <ogmonedero@gmail.com>
2023-03-28 19:02:55 +0200

    Recolocación del grid

--- js/custom.js
+++ js/custom.js
@@ -328,31 +328,30 @@
 
 
 
-	var portfolioMasonry = function() {
- $('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-          filter: data
-        })
-      });
+	var portfolioMasonry = function () {
+		//$('.grid .all:not(.principal)').css('display', 'none');
+		$('.filters ul li').click(function () {
+			$('.filters ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: data
+			})
+		});
 
 
-      if(document.getElementById("section-portfolio")){
-            var $grid = $(".grid").isotope({
-              itemSelector: ".all",
-              percentPosition: true,
-              masonry: {
-                columnWidth: ".all"
-              }
-            })
-      };
-
-
+		if (document.getElementById("section-portfolio")) {
+			var $grid = $(".grid").isotope({
+				itemSelector: ".all",
+				percentPosition: true,
+				masonry: {
+					columnWidth: ".all"
+				}
+			})
+		};
 	};
-
+	  
 
 	$(function(){
 
